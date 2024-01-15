f546fe1007271256b33053c8445cb1fdc4447fa3
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-01-15 22:01:27 +0000

    Differentiation between ports on Unix-like vs Windows
    Port can be manually set by setting the class variable. Should perhaps go in the constructor instead?

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,17 +1,26 @@
 import aiohttp_jinja2
 import jinja2
+import os
 
 from aiohttp import web
 from arduino import Arduino
 
 
+
+
 class Webserver(web.Application):
+    if os.name ==  "nt":
+        ARDUINO_PORT = "COM1"
+    else:
+        ARDUINO_PORT = "/dev/ttyACM0"
+
+
     def __init__(self, templates_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         aiohttp_jinja2.setup(self, loader=jinja2.FileSystemLoader(templates_path))
 
-        self.ard = Arduino("/dev/ttyACM0", 115200)
+        self.ard = Arduino(Webserver.ARDUINO_PORT, 115200)
 
 
         self.add_routes([
