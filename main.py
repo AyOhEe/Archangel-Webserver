4250bd3c475902f3d96d5b8f0352e60e861db7ec
AwesomePantsAP <awesomepantsap@gmail.com>
2024-01-15 21:38:44 +0000

    Webserver added
    Basic control and communication with Arduino. Currently sets the rate at which the on-board LED flashes.

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-from arduino import Arduino
-
-import time
-
-
-ARDUINO_PORT = "/dev/ttyACM0"
-ARDUINO_BAUD = 115200
-SERIAL_KWARGS = {"timeout": 0.1}
-
-
-ARDUINO_INSTANCE = None
-def main():
-    global ARDUINO_INSTANCE
-
-    #connect to the adruino
-    ARDUINO_INSTANCE = Arduino(ARDUINO_PORT, ARDUINO_BAUD, serial_kwargs=SERIAL_KWARGS)
-
-    value = 0
-    while True:
-        time.sleep(0.3)
-        value = (value + 1) % 256
-        ARDUINO_INSTANCE.try_send_message(bytes([value, ord("\n")]))
+from webserver import Webserver
+from aiohttp import web
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        ARDUINO_INSTANCE.shutdown()
+    server = Webserver("web/")
+    web.run_app(server)
---
+++ webserver/webserver.py
@@ -0,0 +1,36 @@
+import aiohttp_jinja2
+import jinja2
+
+from aiohttp import web
+from arduino import Arduino
+
+
+class Webserver(web.Application):
+    def __init__(self, templates_path, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        aiohttp_jinja2.setup(self, loader=jinja2.FileSystemLoader(templates_path))
+
+        self.ard = Arduino("/dev/ttyACM0", 115200)
+
+
+        self.add_routes([
+            web.get("/", self.index),
+            web.post("/change_blink", self.change_blink)
+        ])
+
+    async def index(self, request):
+        context = {}
+        response = aiohttp_jinja2.render_template("index.html",
+                                                  request,
+                                                  context)
+        return response
+
+    async def change_blink(self, request):
+        blink_rate = (await request.post())["blinkrate"]
+        self.ard.try_send_message(blink_rate)
+        raise web.HTTPFound("/")
+
+if __name__ == "__main__":
+    server = Webserver("../web")
+    web.run_app(server)
