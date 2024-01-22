2a8bf3b53ab6bb4d516ee9648b6f6f80bba24d94
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-01-22 14:31:37 +0000

    Commandline arguments for port and dummy
    You shouldn't have to go in an modify the code to do something like that. This is infinitely better

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from webserver import Webserver
 from aiohttp import web
 
+import argparse
 
-Webserver.ARDUINO_PORT = "COM3"
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='Archangel Webserver',                             
+        description='Hosts a website used to communicate with the attached arduino'
+    )
+    parser.add_argument("-d", "--usedummy", action='store_true')
+    parser.add_argument("-p", "--port", default=None)
+    args = parser.parse_args()
+
+    if args.usedummy:   
+        print("Using DummyArduino class")
+        Webserver.USE_DUMMY_ARDUINO = True
+
+    if args.port:
+        print(f"Using Arduino port: {args.port}")
+        Webserver.ARDUINO_PORT = args.port
+
+
     server = Webserver("web/")
     web.run_app(server)
